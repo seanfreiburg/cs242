74b4e245d712a8059df989b138b59b3a558a4653
Sean Freiburg <frycicle@gmail.com>
2013-10-06 16:30:06 -0500

    a bunch of db work

--- rails_portfolio/config/routes.rb
+++ rails_portfolio/config/routes.rb
@@ -1,4 +1,13 @@
 RailsPortfolio::Application.routes.draw do
+  resources :file_versions
+
+
+  resources :file_records
+
+
+  resources :projects
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +57,8 @@ RailsPortfolio::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'static_pages#home'
+  resources :projects
 
   # See how all your routes lay out with "rake routes"
 
