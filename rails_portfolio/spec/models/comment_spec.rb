3736cf22f0103bae6756c0530515ce22a8162a36
Sean Freiburg <frycicle@gmail.com>
2013-10-16 02:11:40 -0500

    added testing

---
+++ rails_portfolio/spec/models/comment_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe Comment do
+
+  before(:each) do
+    @project = Project.create(title: 'test project')
+  end
+
+  describe "test comments" do
+    it "make a normal comment" do
+      comment = Comment.build_from(@project, 0, 'sadsadas')
+      comment.save
+      @project.root_comments.size.should be(1)
+      @project.root_comments.first.body.should eq('sadsadas')
+
+    end
+
+    it "make a multiple root comments" do
+      comment = Comment.build_from(@project, 0, 'sadsadas')
+      comment.save
+      comment = Comment.build_from(@project, 0, 'sadsjklkadas')
+      comment.save
+      @project.root_comments.size.should be(2)
+      @project.root_comments.first.body.should eq('sadsadas')
+      @project.root_comments.last.body.should eq('sadsjklkadas')
+
+    end
+
+    it "make a children comments" do
+      comment1 = Comment.build_from(@project, 0, 'sadsadas')
+      comment1.save
+      comment2 = Comment.build_from(@project, 0, 'child')
+      comment2.save
+      comment3 = Comment.build_from(@project, 0, 'child2')
+      comment3.save
+      comment2.move_to_child_of(comment1)
+      comment3.move_to_child_of(comment1)
+
+      comment1.children.size.should be 2
+      comment1.children.first.body.should eq('child')
+      comment1.children.last.body.should eq('child2')
+
+    end
+
+  end
+
+
+end
--- rails_portfolio/spec/models/project_spec.rb
+++ rails_portfolio/spec/models/project_spec.rb
@@ -2,4 +2,36 @@ require 'spec_helper'
 
 describe Project do
 
+  before(:each) do
+    load "#{Rails.root}/db/seeds.rb"
+  end
+
+  describe "test data import" do
+    it "gets gitignore project" do
+      project = Project.find_by_title('.gitignore')
+      project.title.should eq('.gitignore')
+    end
+
+    it "gets Assignment1 project" do
+      project = Project.find_by_title('Assignment1')
+      project.title.should eq('Assignment1')
+    end
+
+    it "gets HW0 project" do
+      project = Project.find_by_title('HW0')
+      project.title.should eq('HW0')
+    end
+
+    it "gets csAir project" do
+      project = Project.find_by_title('csAir')
+      project.title.should eq('csAir')
+    end
+
+    it "gets rails_portfolio project" do
+      project = Project.find_by_title('rails_portfolio')
+      project.title.should eq('rails_portfolio')
+    end
+  end
+
+
 end
