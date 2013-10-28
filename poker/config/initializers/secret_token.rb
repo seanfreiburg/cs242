7fecbd850aa26f7bc60e05ef12468d1aec20bccc
Sean Freiburg <frycicle@gmail.com>
2013-10-28 14:27:05 -0500

    added poker

---
+++ poker/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Poker::Application.config.secret_key_base = '3085d33bebefab8bf3a8a82964280fe8aaf57f845ac9bb9c33f12e2d99afa59b409f4a7e88ba415827c30ab4894a21ae897ad2d6bdafe22c0c9dba2a5309feda'
