9698bd2080afc1c90dc5c39f65f5bdf5511a7ce5
Sean Freiburg <frycicle@gmail.com>
2013-11-05 23:40:48 -0600

    can register and get an api key

--- poker/config/routes.rb
+++ poker/config/routes.rb
@@ -53,4 +53,6 @@ Poker::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  resources :players, only: [:show, :create,:new ]
 end
