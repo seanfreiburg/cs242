7b137154f218366a169fdd615ad06a22c5f1a1b2
Brian Hauger <haugerbr@gmail.com>
2013-11-10 14:11:35 -0600

    Community Cards and Animation

--- poker/lib/pokermodel/assets/javascript/pokerview.js
+++ poker/lib/pokermodel/assets/javascript/pokerview.js
@@ -2,12 +2,20 @@ $(document).ready(function(){
 
 $('.card_back').hide();
 
+var blah = function(){
+$('#0').fadeOut(1000,function(){
+	$('#0').removeClass('card_back');
+	$('#0').addClass('ace_hearts');
+	$('#0').fadeIn(1000);
+});
+
+}
 
 $('#0').fadeIn(1000,function(){
 	$('#1').fadeIn(1000,function(){
 		$('#2').fadeIn(1000,function(){
 			$('#3').fadeIn(1000,function(){
-				$('#4').fadeIn(1000)
+				$('#4').fadeIn(1000,blah)
 			});
 		});
 	});
