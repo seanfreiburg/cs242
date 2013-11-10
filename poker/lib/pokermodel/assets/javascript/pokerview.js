ccce27ff7824c9bf7867b33dd1fd7cac42768ffa
Brian Hauger <haugerbr@gmail.com>
2013-11-10 01:32:11 -0600

    Added Jquery

---
+++ poker/lib/pokermodel/assets/javascript/pokerview.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+$('.card_back').hide();
+
+
+$('#0').fadeIn(1000,function(){
+	$('#1').fadeIn(1000,function(){
+		$('#2').fadeIn(1000,function(){
+			$('#3').fadeIn(1000,function(){
+				$('#4').fadeIn(1000)
+			});
+		});
+	});
+});
+
+
+
+});
