13c5cdeaa1b5be2747de34ad695f54d78407cff6
Brian Hauger <haugerbr@gmail.com>
2013-11-10 21:43:16 -0600

    Changed animation and background

--- poker/lib/pokermodel/assets/javascript/pokerview.js
+++ poker/lib/pokermodel/assets/javascript/pokerview.js
@@ -3,19 +3,19 @@ $(document).ready(function(){
 $('.card_back').hide();
 
 var blah = function(){
-$('#0').fadeOut(1000,function(){
+$('#0').fadeOut(500,function(){
 	$('#0').removeClass('card_back');
 	$('#0').addClass('ace_hearts');
-	$('#0').fadeIn(1000);
+	$('#0').fadeIn(500);
 });
 
 }
 
-$('#0').fadeIn(1000,function(){
-	$('#1').fadeIn(1000,function(){
-		$('#2').fadeIn(1000,function(){
-			$('#3').fadeIn(1000,function(){
-				$('#4').fadeIn(1000,blah)
+$('#0').fadeIn(500,function(){
+	$('#1').fadeIn(500,function(){
+		$('#2').fadeIn(500,function(){
+			$('#3').fadeIn(500,function(){
+				$('#4').fadeIn(500,blah)
 			});
 		});
 	});
