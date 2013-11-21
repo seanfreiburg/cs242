54fc8db1f0d5b672e14809892266a079df5269ae
Brian Hauger <haugerbr@gmail.com>
2013-11-21 10:52:08 -0600

    Refactoring Javascript and CSS

--- poker/lib/pokermodel/assets/javascript/pokerview.js
+++ poker/lib/pokermodel/assets/javascript/pokerview.js
@@ -2,25 +2,44 @@ $(document).ready(function(){
 
 $('.card_back').hide();
 
-var blah = function(){
-$('#0').fadeOut(500,function(){
-	$('#0').removeClass('card_back');
-	$('#0').addClass('ace_hearts');
-	$('#0').fadeIn(500);
-});
-
-}
-
-$('#0').fadeIn(500,function(){
-	$('#1').fadeIn(500,function(){
-		$('#2').fadeIn(500,function(){
-			$('#3').fadeIn(500,function(){
-				$('#4').fadeIn(500,blah)
+var dealCommunity = function(){
+	$('#0').fadeIn(500,function(){
+		$('#1').fadeIn(500,function(){
+			$('#2').fadeIn(500,function(){
+				$('#3').fadeIn(500,function(){
+					$('#4').fadeIn(500);
+				});
 			});
 		});
 	});
-});
+}
 
+var showFlop = function(card_array){
+	$('#0').fadeOut(500,function(){
+		$('#0').removeClass('card_back');
+		$('#0').addClass(card_array[0]);
+		$('#0').fadeIn(500);
+	});
 
+}
+
+var showTurn = function(card_array){
+	$('#3').fadeOut(500,function(){
+		$('#3').removeClass('card_back');
+		$('#3').addClass(card_array[3]);
+		$('#3').fadeIn(500);
+	});
+}
+
+var showRiver = function(card_array){
+	$('#4').fadeOut(500,function(){
+		$('#4').removeClass('card_back');
+		$('#4').addClass(card_array[4]);
+		$('#4').fadeIn(500);
+	});	
+}
+
+dealCommunity();
+//ShowFlop(['AD','AS','AC']);
 
 });
--- poker/lib/pokermodel/cardCSSGen.rb
+++ poker/lib/pokermodel/cardCSSGen.rb
@@ -1,16 +1,16 @@
 	string = ""
 	x = -1
-	for card in ["ace","two","three","four","five","six","seven","eight","nine","ten","jack","queen","king"]
+	for card in ["A","2","3","4","5","6","7","8","9","T","J","Q","K"]
 		y = -1
-		for suit in ["clubs","spades","hearts","diamonds"]
+		for suit in ["C","S","H","D"]
 			string += "\n"
-			string += "."+card+"_"+suit+"{\n"
+			string += "."+card+suit+"{\n"
 			string += "\twidth:71px;\n"
 			string += "\theight:96px;\n"
-			string += "\tbackground:url(../images/playingcards.png) "+x.to_s+" "+y.to_s+";\n"
+			string += "\tbackground:url(../images/playingcards.png) "+x.to_s+"px "+y.to_s+"px ;\n"
 			string += "}\n"
 			y += -98
 		end
 		x += -73
 	end
-	File.write("cards.css",string)
+	File.write("./assets/stylesheets/cards.css",string)
