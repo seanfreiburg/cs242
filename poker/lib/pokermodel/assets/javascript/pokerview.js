5b38361621318e6db4370cfc9fe2a8c254047e3b
Brian Hauger <haugerbr@gmail.com>
2013-12-05 09:18:52 -0600

    Fuck Yes Deal Animation Working

--- poker/lib/pokermodel/assets/javascript/pokerview.js
+++ poker/lib/pokermodel/assets/javascript/pokerview.js
@@ -9,7 +9,9 @@ var bet_raise_button_handler = function(){
 	}
 	else
 		$('#bet').html('Bet');
+
 	});
+
 }
 
 var bet_slider_handler = function(){
@@ -40,7 +42,9 @@ var start_of_game = function(){
 	$('#bet_slider').hide();
 	$('#amount').hide();
 	 for(i=1; i<9; i++){
-	 	$('#player'+i).css('opacity','0.0');
+	 	$('.hand .well').css('opacity','0.0')
+	 	$('#player'+i+'card1').css('opacity','0.0');
+	 	$('#player'+i+'card2').css('opacity','0.0');
 	 }
 }
 
@@ -64,12 +68,37 @@ var showHand = function(hand_array){
 		});
 }
 
-var deal_players = function(num_players){
 
+
+
+var deal_players = function(num_players){
+	var deal_orders = {2:[1,5],3:[1,2,5],4:[1,2,5,6],
+						5:[1,3,5,7,8],6:[1,2,3,5,6,7],7:[1,2,3,4,5,6,7],8:[1,2,3,4,5,6,7,8]};
+	deal_sequence = deal_orders[num_players];
+	deal_recursive(deal_sequence,0,1);
+		
+	
+}
+
+var deal_recursive = function(sequence,i,j){
+	if(i == sequence.length && j == 2)
+		return;
+	if(i == sequence.length){
+		j=j+1;
+		i=0;
+	}
+	$('.deal_static div').addClass('deal_player'+sequence[i]+'_card'+j)
+	$('.deal_static div').one('webkitAnimationEnd',[deal_sequence,i,j],
+		function(e){
+			$('#player'+e.data[0][e.data[1]]+'card'+e.data[2]).css('opacity','1.0');
+			$('.deal_static div').removeClass();
+			deal_recursive(e.data[0],e.data[1]+1,e.data[2]);
+		});
 }
 
 $(document).ready(function(){
 	start_of_game();
 	bet_raise_button_handler();
 	bet_slider_handler();
+	deal_players(2);
 });
