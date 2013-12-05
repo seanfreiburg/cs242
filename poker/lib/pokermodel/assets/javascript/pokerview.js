f0bc68ef4bac2776e99df437dc958ca12aede464
Brian Hauger <haugerbr@gmail.com>
2013-12-05 11:01:13 -0600

    Created TableView Class

--- poker/lib/pokermodel/assets/javascript/pokerview.js
+++ poker/lib/pokermodel/assets/javascript/pokerview.js
@@ -26,79 +26,11 @@ var bet_slider_handler = function(){
 	});
 }
 
-var changeButtons = function(){
-	if ($('#bet').html() == 'Bet')
-		$('#bet').html('Raise');
-	else
-		$('#bet').html('Bet');
-
-	if ($('#check').html('Check'))
-		$('#check').html('Call');
-	else
-		$('#check').html('Check');
-}
-
-var start_of_game = function(){
-	$('#bet_slider').hide();
-	$('#amount').hide();
-	 for(i=1; i<9; i++){
-	 	$('.hand .well').css('opacity','0.0')
-	 	$('#player'+i+'card1').css('opacity','0.0');
-	 	$('#player'+i+'card2').css('opacity','0.0');
-	 }
-}
-
-var showHand = function(hand_array){
-		$('#player5card1').fadeOut(500,
-		function(){
-			$('#player5card1').removeClass('card_back');
-			$('#player5card1').addClass(hand_array[1]);
-			$('#player5card2').css('opacity','0.0');
-			$('#player5card1').fadeIn(500,
-				function(){
-					$('#player5card2').fadeOut(500,
-						function(){
-							$('#player5card2').removeClass('card_back');
-							$('#player5card2').addClass(hand_array[0]);
-							$('#player5card2').css('opacity','1.0');
-							$('#player5card2').fadeIn(500);
-						});
-				});
-
-		});
-}
-
-
-
-
-var deal_players = function(num_players){
-	var deal_orders = {2:[1,5],3:[1,2,5],4:[1,2,5,6],
-						5:[1,3,5,7,8],6:[1,2,3,5,6,7],7:[1,2,3,4,5,6,7],8:[1,2,3,4,5,6,7,8]};
-	deal_sequence = deal_orders[num_players];
-	deal_recursive(deal_sequence,0,1);
-		
-	
-}
-
-var deal_recursive = function(sequence,i,j){
-	if(i == sequence.length && j == 2)
-		return;
-	if(i == sequence.length){
-		j=j+1;
-		i=0;
-	}
-	$('.deal_static div').addClass('deal_player'+sequence[i]+'_card'+j)
-	$('.deal_static div').one('webkitAnimationEnd',[deal_sequence,i,j],
-		function(e){
-			$('#player'+e.data[0][e.data[1]]+'card'+e.data[2]).css('opacity','1.0');
-			$('.deal_static div').removeClass();
-			deal_recursive(e.data[0],e.data[1]+1,e.data[2]);
-		});
-}
-
 $(document).ready(function(){
-	start_of_game();
+	var table = new TableView(2);
 	bet_raise_button_handler();
 	bet_slider_handler();
-	deal_players(2);
+	table.start_of_round();
+	table.deal_players();
+
 });
---
+++ poker/lib/pokermodel/assets/javascript/tableview.js
@@ -0,0 +1,127 @@
+function TableView(num_players){
+
+	this.num_players = num_players;
+
+	this.changeButtons = function(){
+		if ($('#bet').html() == 'Bet')
+			$('#bet').html('Raise');
+		else
+			$('#bet').html('Bet');
+
+		if ($('#check').html('Check'))
+			$('#check').html('Call');
+		else
+			$('#check').html('Check');
+		}
+
+	this.start_of_round = function(){
+		$('#bet_slider').hide();
+		$('#amount').hide();
+	 	for(i=1; i<9; i++){
+	 		$('.hand .well').css('opacity','0.0')
+	 		$('#player'+i+'card1').css('opacity','0.0');
+	 		$('#player'+i+'card2').css('opacity','0.0');
+	 		if(i < 6)
+	 			$('#community'+(i-1)).hide();
+	 	}
+	}
+
+	var dealCommunity = function(){
+		$('#community0').fadeIn(500,function(){
+			$('#community1').fadeIn(500,function(){
+				$('#community2').fadeIn(500,function(){
+					$('#community3').fadeIn(500,function(){
+						$('#community4').fadeIn(500);
+					});
+				});
+			});
+		});
+	}
+
+	var deal_recursive = function(sequence,i,j){
+		if(i == sequence.length && j == 2){
+			$('.deal_static').hide(dealCommunity());
+			return;
+		}
+		if(i == sequence.length){
+			j=j+1;
+			i=0;
+		}
+		$('.deal_static div').addClass('deal_player'+sequence[i]+'_card'+j)
+		$('.deal_static div').one('webkitAnimationEnd',[deal_sequence,i,j],
+			function(e){
+				$('#player'+e.data[0][e.data[1]]+'card'+e.data[2]).css('opacity','1.0');
+				$('.deal_static div').removeClass();
+				deal_recursive(e.data[0],e.data[1]+1,e.data[2]);
+			});
+	}
+
+	this.deal_players = function(){
+		var deal_orders = {2:[1,5],3:[1,2,5],4:[1,2,5,6],
+							5:[1,3,5,7,8],6:[1,2,3,5,6,7],7:[1,2,3,4,5,6,7],8:[1,2,3,4,5,6,7,8]};
+		deal_sequence = deal_orders[this.num_players];
+		deal_recursive(deal_sequence,0,1);
+		
+	}
+
+	this.showHand = function(hand_array){
+		$('#player5card1').fadeOut(500,
+		function(){
+			$('#player5card1').removeClass('card_back');
+			$('#player5card1').addClass(hand_array[1]);
+			$('#player5card2').css('opacity','0.0');
+			$('#player5card1').fadeIn(500,
+				function(){
+					$('#player5card2').fadeOut(500,
+						function(){
+							$('#player5card2').removeClass('card_back');
+							$('#player5card2').addClass(hand_array[0]);
+							$('#player5card2').css('opacity','1.0');
+							$('#player5card2').fadeIn(500);
+						});
+				});
+
+		});
+	}
+
+	this.showFlop = function(card_array){
+		$('#community0').fadeOut(500,function(){
+			$('#community0').removeClass('card_back');
+			$('#community0').addClass(card_array[0]);
+			$('#community0').fadeIn(500,	
+				function(){
+					$('#community1').fadeOut(500,function(){
+						$('#community1').removeClass('card_back');
+						$('#community1').addClass(card_array[1]);
+						$('#community1').fadeIn(500,	
+							function(){
+								$('#community2').fadeOut(500,function(){
+									$('#community2').removeClass('card_back');
+									$('#community2').addClass(card_array[2]);
+									$('#community2').fadeIn(500);
+								});
+							});
+					});
+				});
+		});
+
+
+	}
+
+	this.showTurn = function(card_array){
+		$('#community3').fadeOut(500,function(){
+			$('#community3').removeClass('card_back');
+			$('#community3').addClass(card_array[3]);
+			$('#community3').fadeIn(500);
+		});
+	}
+
+	this.showRiver = function(card_array){
+		$('#community4').fadeOut(500,function(){
+			$('#community4').removeClass('card_back');
+			$('#community4').addClass(card_array[4]);
+			$('#community4').fadeIn(500);
+		});	
+	}
+
+}
