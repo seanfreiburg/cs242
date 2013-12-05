17b5dea2359c05054f3c2f9ad472a953943b6344
Brian Hauger <haugerbr@gmail.com>
2013-12-04 22:43:50 -0600

    Finalized Show Hand

--- poker/lib/pokermodel/assets/javascript/pokerview.js
+++ poker/lib/pokermodel/assets/javascript/pokerview.js
@@ -24,12 +24,48 @@ var bet_slider_handler = function(){
 	});
 }
 
+var changeButtons = function(){
+	if ($('#bet').html() == 'Bet')
+		$('#bet').html('Raise');
+	else
+		$('#bet').html('Bet');
+
+	if ($('#check').html('Check'))
+		$('#check').html('Call');
+	else
+		$('#check').html('Check');
+}
+
 var start_of_game = function(){
 	$('#bet_slider').hide();
 	$('#amount').hide();
 	 for(i=1; i<9; i++){
 	 	$('#player'+i).css('opacity','0.0');
 	 }
+}
+
+var showHand = function(hand_array){
+		$('#player5card1').fadeOut(500,
+		function(){
+			$('#player5card1').removeClass('card_back');
+			$('#player5card1').addClass(hand_array[1]);
+			$('#player5card2').css('opacity','0.0');
+			$('#player5card1').fadeIn(500,
+				function(){
+					$('#player5card2').fadeOut(500,
+						function(){
+							$('#player5card2').removeClass('card_back');
+							$('#player5card2').addClass(hand_array[0]);
+							$('#player5card2').css('opacity','1.0');
+							$('#player5card2').fadeIn(500);
+						});
+				});
+
+		});
+}
+
+var deal_players = function(num_players){
+
 }
 
 $(document).ready(function(){
