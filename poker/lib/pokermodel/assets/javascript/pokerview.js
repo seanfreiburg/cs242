6d48796797d3a6e81653d3d95a51c2051fbd462f
Brian Hauger <haugerbr@gmail.com>
2013-12-05 14:45:57 -0600

    Started integrating api

--- poker/lib/pokermodel/assets/javascript/pokerview.js
+++ poker/lib/pokermodel/assets/javascript/pokerview.js
@@ -1,3 +1,20 @@
+var key = null;
+var api = new PokerApi('http://0.0.0.0:3000');
+
+
+var modal_close_handler = function(){
+
+	$('#modal_close').click(function(){
+		key = api.get_key('Anon'+Math.floor((Math.random()*1000)+1));
+	});
+}
+
+var modal_submit_handler = function(){
+	$('#modal_submit').click(function(){
+		key = api.get_key($('.modal-body input').val());
+	});
+}
+
 var bet_raise_button_handler = function(){
  $('#bet').click(function(){
 	$('#bet_slider').toggle();
@@ -26,10 +43,20 @@ var bet_slider_handler = function(){
 	});
 }
 
+var gameloop = function(num_players){
+	var table = new TableView(num_players);
+
+	table.deal_players();
+}
+
 $(document).ready(function(){
-	var table = new TableView(4);
+	var table = new TableView(2);
 	bet_raise_button_handler();
 	bet_slider_handler();
+	modal_submit_handler();
+	modal_close_handler();
 	table.start_of_round();
-	table.deal_players();
+	$('#name input').val('Anon'+Math.floor((Math.random()*1000)+1));
+	$('#name').modal('show');
 });
+
